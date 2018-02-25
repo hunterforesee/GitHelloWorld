5cf3fa1b690218a0d4be323d7183fa886b5d8a29
hunterforesee <hunterforesee@163.com>
2018-02-25 12:47:31 +0800

    SubFunction 提交

--- GitHelloWorld/Program.cs
+++ GitHelloWorld/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 
 namespace GitHelloWorld
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            Console.WriteLine("你好");
+            Console.ReadKey();
+            IPAddress address = IPAddress.Parse("192.168.2.103");
         }
+
+
+        private static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
     }
 }
