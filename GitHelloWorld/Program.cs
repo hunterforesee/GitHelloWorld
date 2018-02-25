7d0d33fd7ce96ec11e19ccedc85bd7548151fc3d
hunterforesee <hunterforesee@163.com>
2018-02-25 12:57:42 +0800

    socket

--- GitHelloWorld/Program.cs
+++ GitHelloWorld/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
 
 namespace GitHelloWorld
 {
@@ -10,6 +12,12 @@ namespace GitHelloWorld
             Console.WriteLine("你好");
             Console.ReadKey();
             IPAddress address = IPAddress.Parse("192.168.2.103");
+            Socket sockerserver = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            sockerserver.Bind(new IPEndPoint(address,7788));
+
+            sockerserver.Listen(100);
+
+            Socket client = sockerserver.Accept();
         }
 
 
